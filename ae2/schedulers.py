abad52d7957510a5cbaa8e872a9d48efa4052d45
renzhexiangjiao <franeksowul@gmail.com>
2020-11-14 17:02:54 +0000

    indicate places where code should be updated

--- ae2/schedulers.py
+++ ae2/schedulers.py
@@ -3,31 +3,39 @@ from des import SchedulerDES
 
 class FCFS(SchedulerDES):
     def scheduler_func(self, cur_event):
+        #TODO
         pass
 
     def dispatcher_func(self, cur_process):
+        #TODO
         pass
 
 
 class SJF(SchedulerDES):
     def scheduler_func(self, cur_event):
+        #TODO
         pass
 
     def dispatcher_func(self, cur_process):
+        #TODO
         pass
 
 
 class RR(SchedulerDES):
     def scheduler_func(self, cur_event):
+        #TODO
         pass
 
     def dispatcher_func(self, cur_process):
+        #TODO
         pass
 
 
 class SRTF(SchedulerDES):
     def scheduler_func(self, cur_event):
+        #TODO
         pass
 
     def dispatcher_func(self, cur_process):
+        #TODO
         pass
