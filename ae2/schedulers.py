7a002aa6f1f27134549cbbaf073599544d6da249
renzhexiangjiao <franeksowul@gmail.com>
2020-11-24 23:39:45 +0100

    20 lines

--- ae2/schedulers.py
+++ ae2/schedulers.py
@@ -1,59 +1,23 @@
 from des import SchedulerDES
 from event import Event, EventTypes
 from process import Process, ProcessStates
-
-class FCFS(SchedulerDES):
-    """FCFS(First Come First Serve) is a non-preemptive scheduling algorithm,
-    meaning that the processes are executed without interruption.
-    Processes are scheduled in order of their arrival.
-    """
-    def scheduler_func(self, cur_event):
-        return self.processes[cur_event.process_id]
-
-    def dispatcher_func(self, cur_process):
-        time_run_for = cur_process.run_for(cur_process.service_time, self.time)
-        cur_process.process_state = ProcessStates.TERMINATED
-        return Event(process_id=cur_process.process_id,
-                    event_time=self.time + time_run_for,
-                    event_type=EventTypes.PROC_CPU_DONE)
-
-
-class SJF(FCFS):
-    """SJF(Shortest Job First) is a non-preemptive scheduling algorithm,
-    meaning that the processes are executed without interruption.
-    Processes are scheduled according to their service time,
-    with the shortest one first.
-    """
-    def scheduler_func(self, cur_event):
-        return sorted(filter(lambda p : p.process_state==ProcessStates.READY, self.processes), key=lambda p : p.service_time)[0]
-
-
 class RR(SchedulerDES):
-    """RR(Round Robin) is a preemptive scheduling algorithm,
-    meaning that the process currently running on the CPU 
-    can be interrupted in favor of another process.
-    It interleaves the execution of processes which are not serviced yet.
-    """
     def scheduler_func(self, cur_event):
         return self.processes[cur_event.process_id]
-
     def dispatcher_func(self, cur_process):
         time_run_for = cur_process.run_for(self.quantum, self.time)
         cur_process.process_state = ProcessStates.READY if cur_process.remaining_time > 0 else ProcessStates.TERMINATED
-        return Event(process_id=cur_process.process_id,
-                    event_time=self.time + time_run_for,
-                    event_type=EventTypes.PROC_CPU_REQ if cur_process.remaining_time > 0 else EventTypes.PROC_CPU_DONE)
-
-
+        return Event(process_id=cur_process.process_id, event_time=self.time + time_run_for, event_type=EventTypes.PROC_CPU_REQ if cur_process.remaining_time > 0 else EventTypes.PROC_CPU_DONE)
+class FCFS(RR):
+    def dispatcher_func(self, cur_process):
+        self.quantum = cur_process.service_time
+        return super().dispatcher_func(cur_process)
+class SJF(FCFS):
+    def scheduler_func(self, cur_event):
+        return sorted(filter(lambda p : p.process_state==ProcessStates.READY, self.processes), key=lambda p : p.service_time)[0]
 class SRTF(RR):
-    """SRTF(Shortest Remaining Time First) is a preemptive scheduling algorithm,
-    meaning that the process currently running on the CPU 
-    can be interrupted in favor of another process. 
-    It prioritises processes which expect to be serviced sooner.
-    """
     def scheduler_func(self, cur_event):
         return sorted(filter(lambda p : p.process_state==ProcessStates.READY, self.processes), key=lambda p : p.remaining_time)[0]
-
     def dispatcher_func(self, cur_process):
         self.quantum = self.next_event_time()-self.time
         return super().dispatcher_func(cur_process)
